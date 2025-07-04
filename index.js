7cee7e248dbf5b45b11341904da915643c1ca8c7
Rishabh Varshney <rishabh.varshney@runo.in>
2025-07-04 17:11:49 +0530

    added logs

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/ivr', (req, res) => {
-    const customer = req.query.customer;
 
-    if (!customer || !/^\d{10}$/.test(customer)) {
-        return res.status(400).send('Invalid customer number');
-    }
+  console.log(req);
 
-    const xml = `
+  const customer = req.query.customer;
+
+  if (!customer || !/^\d{10}$/.test(customer)) {
+    return res.status(400).send('Invalid customer number');
+  }
+
+  const xml = `
     <response>
       <playtext>Connecting you to the Runo Customer</playtext>
       <dial>${customer}</dial>
     </response>
   `;
 
-    res.set('Content-Type', 'text/xml');
-    res.send(xml);
+  res.set('Content-Type', 'text/xml');
+  res.send(xml);
 });
 
 app.get('/', (req, res) => {
-    res.send('Runo X Ozonetel');
+  res.send('Runo X Ozonetel');
 });
 
 app.listen(PORT, () => {
-    console.log(`Runo custom IVR server listening on port ${PORT}`);
+  console.log(`Runo custom IVR server listening on port ${PORT}`);
 });
